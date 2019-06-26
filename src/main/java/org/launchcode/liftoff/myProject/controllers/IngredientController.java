52f8f453eeec6692ca07bfcf202edb5be865b0db
Maryam Biazaran <maryam.biazaran@gmail.com>
2019-06-26 14:54:58 -0500

    added form to edit individual ingredients

--- src/main/java/org/launchcode/liftoff/myProject/controllers/IngredientController.java
+++ src/main/java/org/launchcode/liftoff/myProject/controllers/IngredientController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -88,6 +85,18 @@ public class IngredientController {
         }
 
     }
+
+    @RequestMapping(value = "edit/{ingredientId}")
+    public String displayEditForm(Model model,
+                                  @PathVariable int ingredientId) {
+        Ingredient ingredient = ingredientDao.findOne(ingredientId);
+        model.addAttribute("ingredient", ingredient);
+        model.addAttribute("categories", ingredientCategoryDao.findAll());
+        model.addAttribute("title", "Edit Ingredient");
+        return "ingredients/editIngredient";
+    }
+
+
 
 
 }
---
+++ src/main/java/org/launchcode/liftoff/myProject/models/data/RecipeIngredientId.java
@@ -0,0 +1,63 @@
+package org.launchcode.liftoff.myProject.models.data;
+
+import org.launchcode.liftoff.myProject.models.Ingredient;
+import org.launchcode.liftoff.myProject.models.Recipe;
+import org.launchcode.liftoff.myProject.models.RecipeIngredient;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class RecipeIngredientId implements Serializable {
+
+    @ManyToOne
+    @JoinColumn
+    private Recipe recipe;
+
+    @ManyToOne
+    @JoinColumn
+    private Ingredient ingredient;
+
+    public RecipeIngredientId(){  }
+
+    public RecipeIngredientId(Recipe recipe, Ingredient ingredient) {
+        this.recipe = recipe;
+        this.ingredient = ingredient;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    // ========= equals and hashcode ============
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RecipeIngredientId that = (RecipeIngredientId) o;
+        return Objects.equals(recipe, that.recipe) &&
+                Objects.equals(ingredient, that.ingredient);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(recipe, ingredient);
+    }
+}
